6d01202cdc3f4ababa983e9b66e5b175f077a562
linzhanghui <linzhanghui@gmail.com>
2014-01-18 20:08:58 +0800

    welcome java

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome {
+    public static void main(String[] args) {
+        String[] greeting = new String[3];
+        greeting[0]="Welcome to Java";
+        greeting[1]="by lzh";
+        greeting[2]="and github";
+
+        for (String g:greeting)
+            System.out.println(g);
+    }
+}
