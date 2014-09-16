dcc319933c42226c209d75c4d3395aab1be0b71f
linzhanghui <linzhanghui@gmail.com>
2014-09-16 22:12:58 +0800

    Create Vertex.java

---
+++ graphn/Vertex.java
@@ -0,0 +1,12 @@
+package com.zhlin.algorithms.graphn;
+
+public class Vertex {
+	public char label;
+	public boolean wasVisited;
+	
+	public Vertex(char lab)
+	{
+		label = lab;
+		wasVisited = false;
+	}
+}
